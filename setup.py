346326885ee9b3ca937103d8634f1739f780be5a
kirin123kirin <kirin123kirin@gmail.com>
2021-03-05 05:40:39 +0900

    bugfix

--- setup.py
+++ setup.py
@@ -1,31 +1,21 @@
 #!/usr/bin/env python
-
 try:
     from setuptools import setup, Extension
 except:
     from distutils import setup, Extension
 
-ext_modules = [
-    Extension(
-        'cdiffer.bycython',
-        ['_cdiffer.c'],
-        include_dirs=['.'],
-    )
-]
+ext_modules = [Extension('cdiffer',
+        sources = ['_cdiffer.c'],
+        #include_dirs=['.'],
+        )]
 
-setup(
-    name="cdiffer",
+setup(name="cdiffer",
     version='0.0.1',
     description="Usefull differ function with Levenshtein distance",
     long_description='',
     author='kirin123kirin',
     ext_modules=ext_modules,
-    packages=['cdiffer'],
-    package_data={'cdiffer': ['_cdiffer.h']},
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
+    classifiers=['License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-    ],
-)
+        'Programming Language :: Python :: 3',],)
