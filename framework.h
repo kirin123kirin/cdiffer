8d5fdc1547b00ae9c9d9c4386627b85dc7139fa6
kirin123kirin <kirin123kirin@gmail.com>
2021-03-05 05:09:33 +0900

    bugfix

--- framework.h
+++ framework.h
@@ -3,3 +3,17 @@
 #define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
 // Windows ヘッダー ファイル
 #include <windows.h>
+
+#ifndef ___DEF__H__
+#define ___DEF__H__
+
+#if defined(_WIN32) && _MSC_VER <= 1600
+typedef signed __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef signed __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
+#include <stdint.h>
+#endif
+
+#endif
