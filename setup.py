745cfe5de03238ba702da11ca505abe604d2cd1d
kirin123kirin <kirin123kirin@gmail.com>
2021-03-09 15:03:07 +0900

    bugfix

--- setup.py
+++ setup.py
@@ -9,21 +9,25 @@ __version__ = '0.0.9'
 
 try:
     # Edit posix platname for pypi upload error
-    if os.name == "posix" and not ("--plat-name" in sys.argv or "-p" in sys.argv):
+    if os.name == "posix" and any(x.startswith("bdist") for x in sys.argv) \
+            and not ("--plat-name" in sys.argv or "-p" in sys.argv):
+
         if "64" in os.uname()[-1]:
             from _cross_platform import get_platname_64bit
+
             plat = get_platname_64bit()
         else:
             from _cross_platform import get_platname_32bit
+
             plat = get_platname_32bit()
         sys.argv.extend(["--plat-name", plat])
 except (ImportError, ModuleNotFoundError):
     pass
 
 ext_modules = [Extension('cdiffer',
-        sources = ['cdiffer.c'],
-        #include_dirs=['.'],
-        )]
+                         sources=['cdiffer.c'],
+                         # include_dirs=['.'],
+                         )]
 
 # Development Status Example
 #   3 - Alpha
@@ -55,15 +59,15 @@ Operating System :: Unix
 readme = pjoin(dirname(__file__), "README.md")
 
 setup(name="cdiffer",
-   version=__version__,
-   description="Usefull differ function with Levenshtein distance.",
-   long_description_content_type='text/markdown',
-   long_description=codecs.open(readme, encoding="utf-8").read(),
-   url='https://github.com/kirin123kirin/cdiffer',
-   author='kirin123kirin',
-   ext_modules=ext_modules,
-   keywords = ["diff", "comparison", "compare"],
-   license="GPL2",
-   platforms=["Windows", "Linux"],  #, "Mac OS-X", "Unix"
-   classifiers=CF.strip().splitlines(),
-   )
+      version=__version__,
+      description="Usefull differ function with Levenshtein distance.",
+      long_description_content_type='text/markdown',
+      long_description=codecs.open(readme, encoding="utf-8").read(),
+      url='https://github.com/kirin123kirin/cdiffer',
+      author='kirin123kirin',
+      ext_modules=ext_modules,
+      keywords=["diff", "comparison", "compare"],
+      license="GPL2",
+      platforms=["Windows", "Linux"],  # , "Mac OS-X", "Unix"
+      classifiers=CF.strip().splitlines(),
+      )
