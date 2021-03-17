a22f26ecfe20172ad23c23a633d440238b06daa4
kirin123kirin <kirin123kirin@gmail.com>
2021-03-17 18:02:55 +0900

    v0.1.6 test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 import sys
 from os.path import dirname, join as pjoin
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 # Edit posix platname for pypi upload error
 if os.name == "posix" and any(x.startswith("bdist") for x in sys.argv) \
