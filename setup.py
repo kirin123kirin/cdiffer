8e362a774ea3e230a5fd8e833dfd99f881d873be
kirin123kirin <kirin123kirin@gmail.com>
2021-03-17 17:29:32 +0900

    v0.1.5 debug

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import Extension, setup
 import os
 import sys
 import codecs
-from io import StringIO
 from os.path import dirname, join as pjoin
 
 __version__ = '0.1.5'
@@ -57,9 +56,8 @@ Operating System :: Unix
 
 # Readme read or edit
 readme = pjoin(dirname(__file__), "README.md")
-badge = re.compile(r'\[!\[.*?\]\(https://.*?badge\.(?:svg|png)\?branch=([^\)]+)\)\]')
 badge = re.compile(r'(\[!\[.*?\]\(https://.*?badge\.(?:svg|png)\?branch=([^\)]+)\)\])')
-description = StringIO()
+description = ""
 is_change = False
 with codecs.open(readme, encoding="utf-8") as f:
     for line in f:
@@ -68,11 +66,14 @@ with codecs.open(readme, encoding="utf-8") as f:
             for b, k in badge.findall(line):
                 line = line.replace(b, b.replace(k, "v" + __version__))
             is_change = True
-        description.write(line)
+        print(repr(line))
+        description += line
+
+print(description)
 
 if is_change:
     with codecs.open(readme, "w", encoding="utf-8") as f:
-        f.write(description.getvalue())
+        f.write(description)
 
 # for python2.7
 tests = {}
@@ -85,7 +86,7 @@ setup(name="cdiffer",
       version=__version__,
       description="Usefull differ function with Levenshtein distance.",
       long_description_content_type='text/markdown',
-      long_description=description.getvalue(),
+      long_description=description,
       url='https://github.com/kirin123kirin/cdiffer',
       author='kirin123kirin',
       ext_modules=ext_modules,
